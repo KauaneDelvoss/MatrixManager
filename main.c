89d0a26b791b3cfdad2e8808d0680369186f0a94
Kauane Delvoss Ribas <86755715+KauaneDelvoss@users.noreply.github.com>
2024-04-28 21:06:48 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -11,20 +11,23 @@ void showMenu(int matrix[][N], int qtdConjuntos);
 void showMatrix(int matrix[][N], int qtdConjuntos);
 void insertSet(int matrix[][N], int qtdConjuntos);
 int unionSet(int matrix[][N], int qtdConjuntos);
-int searchValue(int item, int matrix[][N], int begin, int end, int row);
+int searchValue(int item, int matrix[][N], int begin, int end, int row, int qtdConjuntos);
 int interSecctionSet(int matrix[][N], int qtdConjuntos);
 
 int main()
 
 {
-	int matrix[M][N], qtdConjuntos = 4;
+	int matrix[M][N], qtdConjuntos = 0;
 
 	srand(time(0));
 
 	for (int m = 0; m < M; m++) {
 		for (int n = 0; n < N; n++) {
-			int aux = n > 1 ? 0 : rand() % 11;
-			matrix[m][n] = aux;
+			// descomentar se quiser preencher com numeros aleatorios a partir da 2 coluna da linha
+			// int aux = n > 1 ? 0 : rand() % 11;
+			// matrix[m][n] = aux;
+
+			matrix[m][n] = 0;
 		}
 	}
 	showMatrix(matrix, qtdConjuntos);
@@ -32,6 +35,11 @@ int main()
 }
 
 void showMatrix(int matrix[][N], int qtdConjuntos) {
+
+	if(qtdConjuntos == 0) {
+		printf("A matriz não possui conjuntos!\n");
+	}
+
 	for (int m = 0; m < qtdConjuntos; m++) {
 		for (int n = 0; n < N; n++) {
 			if (n == 0)
@@ -45,22 +53,22 @@ void showMatrix(int matrix[][N], int qtdConjuntos) {
 void showSet(int matrix[][N], int qtdConjuntos) {
 	int index = 0;
 
-	printf("A matriz possui %i conjuntos. Qual você deseja visualizar?", qtdConjuntos);
+	system("cls");	
+	showMatrix(matrix, qtdConjuntos);
+	printf("A matriz possui %i conjuntos. Qual você deseja visualizar? ", qtdConjuntos);
 	scanf("%i", &index);
 	printf("[ ");
 
 	for (int m = 0; m < N; m++) {
 		printf(" %i ", matrix[index][m]);
 	}
-	printf(" ]");
+	printf(" ]\n");
 }
 
 void showMenu(int matrix[][N], int qtdConjuntos) {
 	int option = 0;
-
-	do
-
-	{
+	
+	do {
 		printf("1 - Inserir um novo conjunto vazio\n");
         printf("2 - Inserir dados a um conjunto\n");
         printf("3 - Remover um conjunto\n");
@@ -75,30 +83,44 @@ void showMenu(int matrix[][N], int qtdConjuntos) {
 		switch (option)
 		{
 			case 1:
+				system("cls");
 				qtdConjuntos = createSet(matrix, qtdConjuntos);
 				break;
 			case 2:
+				system("cls");
 				insertSet(matrix, qtdConjuntos);
 				break;
 			case 3:
+				system("cls");
 				qtdConjuntos = removeSet(matrix, qtdConjuntos);
 				break;
 			case 4:
+				system("cls");
 				qtdConjuntos = unionSet(matrix, qtdConjuntos);
 				break;
 			case 5:
+				system("cls");
 				qtdConjuntos = interSecctionSet(matrix, qtdConjuntos);
 				break;
 			case 6:
+				system("cls");
 				showSet(matrix, qtdConjuntos);
 				break;
 			case 7:
+				system("cls");
 				showMatrix(matrix, qtdConjuntos);
 				break;
 			case 8:
-				searchValue(0, matrix, 0, N - 1, -1);
+				system("cls");
+				int result = searchValue(-2, matrix, 0, N - 1, -2, qtdConjuntos);
+				if (result != -1) {
+					printf("O vetor possui o numero na coluna %i\n!", result);
+				} else {
+					printf("O vetor não possui o numero na coluna %i\n!", result);
+				}
 				break;
 			case 9:
+				system("cls");
 				exit;
 				break;
 			default:
@@ -113,7 +135,7 @@ void showMenu(int matrix[][N], int qtdConjuntos) {
 int createSet(int matrix[][N], int qtdConjuntos) {
 
 	if (qtdConjuntos < M) {
-
+		system("cls");
 		printf("\nAdicionando 1 conjunto a matriz. \n\n");
 		qtdConjuntos++;
 
@@ -140,20 +162,13 @@ int removeSet(int matrix[][N], int qtdConjuntos) {
 
 	int removingSet = M + 1;
 
-	printf("Matriz atual: \n");
+	system("cls");	
 
-	showMatrix(matrix, qtdConjuntos);
-
-	printf("Selecione o index do conjunto que deseja remover: \n");
-
-	scanf("%d", &removingSet);
-
-	if (removingSet > qtdConjuntos - 1 || removingSet < 0) {
-		printf("Valor invalido selecionado, nada foi feito. \n\n");
-
-		return qtdConjuntos;
-	}
-
+	do {
+		printf("Selecione o index do conjunto que deseja remover: \n");
+		showMatrix(matrix, qtdConjuntos);
+		scanf("%i", &removingSet);
+	} while( removingSet < -1 || removingSet > qtdConjuntos);
 
 	for (int m = removingSet; m < qtdConjuntos - 1; m++) {
 		for (int n = 0; n < N; n++) {
@@ -167,16 +182,15 @@ int removeSet(int matrix[][N], int qtdConjuntos) {
 	return qtdConjuntos - 1;
 }
 
-void insertSet(int matrix[][N], int qtdConjuntos)
-
-{
+void insertSet(int matrix[][N], int qtdConjuntos) {
 	int indice, valor;
+	
+	do {
+		printf("Selecione o index que deseja inserir os valores: \n");
+		showMatrix(matrix, qtdConjuntos);
+		scanf("%i", &indice);
+	} while( indice < 0 || indice > qtdConjuntos);
 
-	printf("Selecione o index que deseja inserir os valores: ");
-
-	showMatrix(matrix, qtdConjuntos);
-
-	scanf("%i", &indice);
 
 	for (int i = 0; i < N; i++) {
 		printf("\nValor %i: ", i);
@@ -210,7 +224,7 @@ int unionSet(int matrix[][N], int qtdConjuntos)
 	for (int i = 0; i < N; i++) {
 		if (count > N) {
 
-			printf("Não é possível realizar a união entre conjuntos!");
+			printf("Não é possível realizar a união entre conjuntos!\n");
 			return qtdConjuntos;
 		}
 
@@ -224,7 +238,7 @@ int unionSet(int matrix[][N], int qtdConjuntos)
 	for (int i = 0; i < N; i++) {
 		if (count > N) {
 
-			printf("Não é possível realizar a união entre conjuntos!");
+			printf("Não é possível realizar a união entre conjuntos!\n");
 			return qtdConjuntos;
 		}
 
@@ -268,7 +282,7 @@ int interSecctionSet(int matrix[][N], int qtdConjuntos)
 	for (int i = 0; i < N; i++) {
 		if (count > N) {
 
-			printf("Não é possível realizar a intersecção entre conjuntos!");
+			printf("Não é possível realizar a intersecção entre conjuntos!\n");
 
 			return qtdConjuntos;
 		}
@@ -288,7 +302,7 @@ int interSecctionSet(int matrix[][N], int qtdConjuntos)
 
 	for (int i = 0; i < N; i++) {
 
-		if (searchValue(matrix[first][i], matrix, matrix[second][0], matrix[second][N - 1], second))
+		if (searchValue(matrix[first][i], matrix, matrix[second][0], matrix[second][N - 1], second, qtdConjuntos))
 		{
 			vetor[i] = matrix[first][i];
 
@@ -317,39 +331,54 @@ int interSecctionSet(int matrix[][N], int qtdConjuntos)
 	return conjuntos;
 }
 
-int searchValue(int item, int matrix[][N], int begin, int end, int row)
+int searchValue(int item, int matrix[][N], int begin, int end, int row, int qtdConjuntos)
 
 {
-	int row1 = row;
-
-	if (row1 == -1) {
-		printf("Deseja pesquisar em qual linha? ");
-
-		scanf("%i", &row1);
-
-		if (row1 < 0 || row1 >= N)
-		{
-			printf("Linha inválida.\n");
-
-			return -1;
-		}
-	}
-
 	int i = (begin + end) / 2;
 
-	if (begin > end) { 
+	if (row == -2) {
+		do {
+			system("cls");	
+			showMatrix(matrix, qtdConjuntos);
+			printf("Deseja pesquisar em qual linha? ");
+			scanf("%i", &row);
+		} while( row < -1 || row >= qtdConjuntos);
+	}
 
+	if (item == -2) {
+		do {
+			system("cls");	
+			printf("Deseja pesquisar qual item? ");
+			scanf("%i", &item);
+		} while( item <= -1);
+	}
+
+	if (begin > end) { 
 		return -1;
 	}
 
-	if (matrix[row1][i] == item && item != 0) { 
+	for (int j = begin; j <= end; j++)
+    {
+        int minIndex = j;
+        for (int k = j + 1; k <= end; k++)
+        {
+            if (matrix[row][k] < matrix[row][minIndex])
+            {
+                minIndex = k;
+            }
+        }
+        int temp = matrix[row][j];
+        matrix[row][j] = matrix[row][minIndex];
+        matrix[row][minIndex] = temp;
+    }
 
-		return i;
+	if (matrix[row][i] == item) { 
+		return i + 1;
 	}
 
-	if (matrix[row1][i] < item) { 
-		return searchValue(item, matrix, i + 1, end, row1);
+	if (matrix[row][i] < item) { 
+		return searchValue(item, matrix, i + 1, end, row, qtdConjuntos);
 	} else { 
-		return searchValue(item, matrix, i - 1, end, row1);
+		return searchValue(item, matrix, i - 1, end, row, qtdConjuntos);
 	}
 }
